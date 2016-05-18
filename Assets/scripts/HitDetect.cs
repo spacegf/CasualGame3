34a6f13321401a47462720ddc46dd8ac6dd48d9d
Isaac Selleck <ixs5214@g.rit.edu>
2016-05-18 14:24:15 -0400

    FIXED ALL PROBLEMS!!!!!!!  no endless respawn, can move/jump at any point, and can always kill each other. Also fixed all the null pointer exceptions

--- Assets/scripts/HitDetect.cs
+++ Assets/scripts/HitDetect.cs
@@ -5,9 +5,12 @@ public class HitDetect : MonoBehaviour {
 
 	public GameObject player;
 
+	private bool startRe = false;
+
 	void Start(){		
 		
 	}
+
 
 
 	void OnTriggerEnter(Collider col){
@@ -16,8 +19,11 @@ public class HitDetect : MonoBehaviour {
 
 		case "Player1":
 			Destroy (col.gameObject);
-			StartCoroutine ("respawnP1");
-	
+			startRe = true;
+
+			if (startRe == true) {
+				StartCoroutine ("respawnP1");
+			}
 			PlayerMovement.p2Kills += 1;
 
 			Debug.Log (PlayerMovement.p2Kills);
@@ -26,8 +32,11 @@ public class HitDetect : MonoBehaviour {
 
 		case "Player2":
 			Destroy (col.gameObject);
-			StartCoroutine ("respawnP2");
-		
+			startRe = true;
+
+			if (startRe == true) {
+				StartCoroutine ("respawnP2");
+			}
 			PlayerMovement.p1Kills += 1;
 
 			Debug.Log (PlayerMovement.p1Kills);
@@ -35,15 +44,23 @@ public class HitDetect : MonoBehaviour {
 
 		case "Player1(Clone)":
 
-
+			Destroy (col.gameObject);
 			PlayerMovement.p2Kills += 1;
-			StartCoroutine ("respawnP1");
+			startRe = true;
+
+			if (startRe == true) {
+				StartCoroutine ("respawnP1");
+			}
 			Debug.Log (PlayerMovement.p2Kills);
 			break;
 
 		case "Player2(Clone)":
 			Destroy (col.gameObject);
-			StartCoroutine ("respawnP2");
+			startRe = true;
+
+			if (startRe == true) {
+				StartCoroutine ("respawnP2");
+			}
 
 			Debug.Log (PlayerMovement.p1Kills);
 			PlayerMovement.p1Kills += 1;
@@ -58,6 +75,7 @@ public class HitDetect : MonoBehaviour {
 	IEnumerator respawnP2(){
 		yield return new WaitForSeconds (2f);
 		GameObject newPlayer2 = (GameObject)Instantiate (player, new Vector3(14.9f,-2.02f,0f), Quaternion.identity);
+		startRe = false;
 
 	}
 
@@ -65,6 +83,7 @@ public class HitDetect : MonoBehaviour {
 	IEnumerator respawnP1(){
 		yield return new WaitForSeconds (2f);
 		GameObject newPlayer1 = (GameObject)Instantiate (player, new Vector3(-14.5f,-2.02f,0f), Quaternion.identity);
+		startRe = false;
 
 	}
 }
